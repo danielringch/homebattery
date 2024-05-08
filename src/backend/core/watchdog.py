1ab1d7eec0d79817fc92ba0f8d8c364a4cc50cca
Daniel Ring <danielring.ch@googlemail.com>
2024-05-08 08:10:25 +0200

    Move watchdog disable pin from SW1 to SW2

--- src/backend/core/watchdog.py
+++ src/backend/core/watchdog.py
@@ -7,7 +7,7 @@ class Watchdog:
             pass
 
     def __init__(self):
-        enabled = bool(Pin(9, Pin.IN).value())
+        enabled = bool(Pin(8, Pin.IN).value())
         log.info(f'Watchdog enabled={enabled}')
         self.__wdt = WDT(timeout=5000) if enabled else self.Stub()
 
