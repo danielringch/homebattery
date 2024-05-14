4b4953f750f634709783c7c1603f1aa79b0c28b2
Daniel Ring <danielring.ch@googlemail.com>
2024-05-14 23:04:20 +0200

    Optimize socket

--- src/backend/core/microsocket.py
+++ src/backend/core/microsocket.py
@@ -55,25 +55,25 @@ class MicroSocket:
             self.__socket.close()
             self.__connected = False
         
-        async def receive(self, length):
+        async def receive_into(self, buffer, offset, length):
             async with self.__receive_lock:
                 self.__check_socket()
 
-                data = bytearray(length)
-                buffer = memoryview(data)
-                received_bytes_count = 0
+                view = memoryview(buffer)
+                bytes_remaining = length
                 start = ticks_ms()
-                while received_bytes_count < length and ticks_diff(ticks_ms(), start) < self.__timeout and self.__connected:
+                while bytes_remaining > 0 and ticks_diff(ticks_ms(), start) < self.__timeout and self.__connected:
                     try:
-                        chunk_size = self.__socket.readinto(buffer[received_bytes_count:], length - received_bytes_count)
+                        chunk_size = self.__socket.readinto(view[offset:], bytes_remaining)
                         if chunk_size is not None:
-                            received_bytes_count += chunk_size
+                            bytes_remaining -= chunk_size
+                            offset += chunk_size
                     except OSError as e:
                         self.__handle_socket_exception(e, 'receive')
                     await sleep(0.05)
 
-                if received_bytes_count >= length:
-                    return data
+                if bytes_remaining <= 0:
+                    return offset
                 elif not self.__connected:
                     raise MicroSocketClosedExecption()
                 else:
@@ -98,6 +98,26 @@ class MicroSocket:
                     raise MicroSocketClosedExecption()
                 else:
                     raise MicroSocketTimeoutException()
+                
+        async def receiveone(self):
+            async with self.__receive_lock:
+                self.__check_socket()
+
+                start = ticks_ms()
+                data = None
+                while ticks_diff(ticks_ms(), start) < self.__timeout and self.__connected and not data:
+                    try:
+                        data = self.__socket.read(1)
+                    except OSError as e:
+                        self.__handle_socket_exception(e, 'receive')
+                    await sleep(0.05)
+
+                if data:
+                    return data
+                elif not self.__connected:
+                    raise MicroSocketClosedExecption()
+                else:
+                    raise MicroSocketTimeoutException()
             
         def empty_receive_queue(self):
             self.__check_socket()
@@ -107,20 +127,21 @@ class MicroSocket:
             async with self.__send_lock:
                 self.__check_socket()
             
-                data = memoryview(data)
                 if length:
                     data = data[:length]
                 else:
                     length = len(data)
-                while len(data) > 0 and self.__connected:
+                while length > 0 and self.__connected:
                     try:
                         chunk_size = self.__socket.write(data)
-                        data = data[chunk_size:]
+                        length -= chunk_size
+                        if length > 0:
+                            data = data[chunk_size:]
                     except OSError as e:
                         self.__handle_socket_exception(e, 'send')
                     await sleep(0.05)
 
-                if len(data) > 0:
+                if length > 0:
                     raise MicroSocketTimeoutException()
                 elif not self.__connected:
                     raise MicroSocketClosedExecption()
