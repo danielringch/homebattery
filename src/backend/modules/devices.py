4791546766bbe7c76641fec87559767182626c9a
Daniel Ring <danielring.ch@googlemail.com>
2024-07-10 20:30:15 +0200

    Add support for consumption data via http request

--- src/backend/modules/devices.py
+++ src/backend/modules/devices.py
@@ -4,6 +4,7 @@ from micropython import const
 _AHOY_DTU = const('ahoyDtu')
 _DALY_8S_24V_60A = const('daly8S24V60A')
 _GROWATT_INVERTER_MODBUS = const('growattinvertermodbus')
+_HTTP_CONSUMPTION = const('httpConsumption')
 _JK_BMS_BD4 = const('jkBmsBd4')
 _LLT_POWER_BMS_V4_BLE = const('lltPowerBmsV4Ble')
 _MQTT_BATTERY = const('mqttBattery')
@@ -33,6 +34,10 @@ class Devices:
                 from ..drivers.growattinvertermodbus import GrowattInverterModbus
                 gc_collect()
                 self.__load_device(log, name, GrowattInverterModbus, meta)
+            elif driver_name == _HTTP_CONSUMPTION:
+                from ..drivers.httpconsumption import HttpConsumption
+                gc_collect()
+                self.__load_device(log, name, HttpConsumption, meta)
             elif driver_name == _JK_BMS_BD4:
                 from ..drivers.jkbmsbd4 import JkBmsBd4
                 gc_collect()
